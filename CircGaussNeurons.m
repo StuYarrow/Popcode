c49828dc0436f4c3287521b2b10eb9fddf08aa10
Stuart Yarrow <stu@outpost.org.uk>
2012-02-03 12:51:52 +0000

    Changes to location of stimulus label variable - moved from subclasses to Neurons class itself.

--- CircGaussNeurons.m
+++ CircGaussNeurons.m
@@ -2,37 +2,66 @@ classdef CircGaussNeurons < Neurons
 	
 	properties
 		width = [];
+        maxRate = [];
+        backgroundRate = [];
 	end
 	
 	methods
 
 		function obj = CircGaussNeurons(varargin)
 		% CIRCGAUSSNEURONS/CIRCGAUSSNEURONS Constructor for CircGaussNeurons object - population of neurons with circular Gaussian tuning curves
-		% obj = CircGaussNeurons(popSize, preferredStimulus, width, maxRate, backgroundRate, integrationTime, variabilityScheme, variabilityOpts)
+		% obj = CircGaussNeurons(preferredStimuli, width, maxRate, backgroundRate, integrationTime, variabilityScheme, variabilityOpts)
 		%
-		% popSize - population size (number of neurons)
-		% dimensionality - stimulus dimensionality (only 1-D stimuli currently supported)
-		% preferredStimulus - preferred stimulus value
+		% preferredStimuli - preferred stimulus value
 		% width - tuning curve width (this would be the variance if the curve was a probability distribution)
 		% maxRate - maximum firing rate (Hz)
 		% backgroundRate - background (spontaneous) firing rate (Hz)
 		% integrationTime - spike counting time per trial
-		% variability - a Variability object
+		% variabilityScheme - type of variability model
 		% variabilityOpts - vector of options
 		%
 		% preferredStimulus, width, maxFiringRate and backgroundFiringRate can be scalars or vectors of length popSize.
-		% CircGaussNeurons accept only 1-D stimuli at present.
+		% CircGaussNeurons accepts only 1-D stimuli at present.
+            
+            switch nargin
+                case 7
+                    preferredStimuli = varargin{1};
+                    widthIn = varargin{2};
+                    maxRateIn = varargin{3};
+                    backgroundRateIn = varargin{4};
+                    integrationTime = varargin{5};
+                    variabilityScheme = varargin{6};
+                    variabilityOpts = varargin{7};
+                otherwise
+                    error('Wrong number of inputs')
+            end
+            
+			% Superclass constructor
+            obj = obj@Neurons(1, preferredStimuli, integrationTime, variabilityScheme, variabilityOpts);
 
-			% Default constructor
-			obj = obj@Neurons(1, varargin{1}, varargin{3}, varargin{4}, varargin{5}, varargin{6}, varargin{7});
-
-			if length(varargin{2}) == 1 & isnumeric(varargin{2})
-				obj.width = double(varargin{2}(ones(obj.popSize, 1)));
-			elseif length(varargin{2}) == obj.popSize & isvector(varargin{2}) & isnumeric(varargin{2})
-				obj.width = reshape(double(), obj.popSize, 1);
+            if isscalar(widthIn) && isnumeric(widthIn)
+				obj.width = double(widthIn(ones(obj.popSize, 1)));
+			elseif length(widthIn) == obj.popSize && isvector(widthIn) && isnumeric(widthIn)
+				obj.width = reshape(double(widthIn), obj.popSize, 1);
 			else
-				error([inputname(3) ' is not a valid maximum firing rate value or vector for population size ' obj.popSize])
-			end
+				error('Invalid width value or vector for population size %n', obj.popSize)
+            end
+            
+            if isscalar(maxRateIn) && isnumeric(maxRateIn)
+				obj.maxRate = double(maxRateIn(ones(obj.popSize, 1)));
+			elseif length(maxRateIn) == obj.popSize && isvector(maxRateIn) && isnumeric(maxRateIn)
+				obj.maxRate = reshape(double(maxRateIn), obj.popSize, 1);
+			else
+				error('Invalid max firing rate value or vector for population size %n', obj.popSize)
+            end
+            
+            if isscalar(backgroundRateIn) && isnumeric(backgroundRateIn)
+				obj.backgroundRate = double(backgroundRateIn(ones(obj.popSize, 1)));
+			elseif length(backgroundRateIn) == obj.popSize && isvector(backgroundRateIn) && isnumeric(backgroundRateIn)
+				obj.backgroundRate = reshape(double(backgroundRateIn), obj.popSize, 1);
+			else
+				error('Invalid background firing rate value or vector for population size %n', obj.popSize)
+            end
 		end
 		
 		function r = meanR(obj, stim)
@@ -40,54 +69,55 @@ classdef CircGaussNeurons < Neurons
 		% r = meanR(obj, stimulusEnsemble)
 		%
 		% r = maxRate * exp(-((stimulus - preferredStimulus)^2 / (2 * width^2))) + backgroundRate
-
-			if ~isa(stim, 'StimulusEnsemble') 
-				error([inputname(2) ' is not a valid StimulusEnsemble object'])
-			end
-
-			if stim.dimensionality ~= 1
-				error('CircGaussNeurons only supports 1-D stimuli at present')
-			end
-
-			stims = repmat(stim.ensemble, [obj.popSize 1]);
-
-			maxRate = repmat(obj.maxRate, 1, stim.n);
-			backgroundRate = repmat(obj.backgroundRate, 1, stim.n);
-			centre = repmat(obj.preferredStimulus, 1, stim.n);
-			width = repmat(obj.width, 1, stim.n);
+            
+            if isa(stim, 'StimulusEnsemble')
+                stims = repmat(stim.ensemble, [obj.popSize 1]);
+                maxRate = repmat(obj.maxRate, [1 stim.n]);
+                backgroundRate = repmat(obj.backgroundRate, [1 stim.n]);
+                centre = repmat(obj.preferredStimulus, [1 stim.n]);
+                width = repmat(obj.width, [1 stim.n]);
+            elseif isa(stim, 'double')
+                stims = repmat(stim(:)', [obj.popSize 1]);
+                maxRate = repmat(obj.maxRate, [1 length(stim)]);
+                backgroundRate = repmat(obj.backgroundRate, [1 length(stim)]);
+                centre = repmat(obj.preferredStimulus, [1 length(stim)]);
+                width = repmat(obj.width, [1 length(stim)]);
+            else
+                error('Invalid stimulus: stim may be a StimulusEnsemble object or vector of stimulus values only')
+            end
 			
 			r = maxRate .* exp(-(1.0 ./ degToRad(width).^2) .* (1 - cosd(double(stims - centre)))) + backgroundRate;
 		end		
 		
 		function dr = dMeanR(obj, stim)
-		%
-		%
-		%
-
-			if ~isa(stim, 'StimulusEnsemble') 
-				error([inputname(2) ' is not a valid StimulusEnsemble object'])
-			end
-
-			if stim.dimensionality ~= 1
-				error('CircGaussNeurons only supports 1-D stimuli at present')
-			end
-
-			stims = repmat(stim.ensemble, [obj.popSize 1]);
-
-			maxRate = repmat(obj.maxRate, 1, stim.n);
-			centre = repmat(obj.preferredStimulus, 1, stim.n);
-			width = repmat(obj.width, 1, stim.n);
-			beta = 1.0 ./ degToRad(width).^2;
-
+		% CIRCGAUSSNEURONS/DMEANR calculates the derivative of the tuning curve
+		% dr dr = dMeanR(obj, stim)
+        
+            if isa(stim, 'StimulusEnsemble')
+                stims = repmat(stim.ensemble, [obj.popSize 1]);
+                maxRate = repmat(obj.maxRate, [1 stim.n]);
+                centre = repmat(obj.preferredStimulus, [1 stim.n]);
+                width = repmat(obj.width, [1 stim.n]);
+            elseif isa(stim, 'double')
+                stims = repmat(stim, [obj.popSize 1]);
+                maxRate = repmat(obj.maxRate, [1 length(stim)]);
+                centre = repmat(obj.preferredStimulus, [1 length(stim)]);
+                width = repmat(obj.width, [1 length(stim)]);
+            else
+                error('Invalid stimulus: stim may be a StimulusEnsemble object or vector of stimulus values only')
+            end
+			
+            beta = 1.0 ./ degToRad(width).^2;
+            
 			dr = pi/180 .* (-maxRate .* exp(-beta) .* beta .* sind(double(stims - centre)) .* exp(beta .* cosd(double(stims - centre))));
-		end
-		
+        end
+        
+        function obj = gainadapt(obj, width, amnt, centre)
+			obj.maxRate = obj.maxRate .* (1 - amnt .* exp(-(1.0 ./ degToRad(width).^2) .* (1 - cosd(double(obj.preferredStimulus - centre)))));
+        end
+        
 		function obj = widthadapt(obj, width, amnt, centre)
-		%
-		%
-		%
-
-			obj = set(obj, 'width', obj.width .* (1 - amnt .* exp(-(1.0 ./ degToRad(width).^2) .* (1 - cosd(double(obj.preferredStimulus - centre))))));
+			obj.width = obj.width .* (1 - amnt .* exp(-(1.0 ./ degToRad(width).^2) .* (1 - cosd(double(obj.preferredStimulus - centre)))));
 		end
 		
 		function obj = remove(obj, nMarg)			
@@ -95,9 +125,17 @@ classdef CircGaussNeurons < Neurons
 			[obj margMask] = remove@Neurons(obj, nMarg);
 			
 			% Deal with subclass properties
-			if length(obj.width) > 1
+            if length(obj.width) > 1
 				obj.width = obj.width(margMask);
-			end
+            end
+            
+            if length(obj.maxRate) > 1
+				obj.maxRate = obj.maxRate(margMask);
+            end
+            
+            if length(obj.backgroundRate) > 1
+				obj.backgroundRate = obj.backgroundRate(margMask);
+            end
 		end
 		
 	end
--- CosNeurons.m
+++ CosNeurons.m
@@ -1,30 +1,48 @@
 classdef CosNeurons < Neurons
 	
 	properties
-		
-	end
+        backgroundRate = [];
+    end
 	
 	methods
 		
 		function obj = CosNeurons(varargin)
-		% COSNEURONS/COSNEURONS Constructor for CosNeurons object - pppulation of neurons with raised cosine tuning curves
-		% obj = CosNeurons(preferredStimulus, backgroundRate, integrationTime, variabilityScheme, variabilityOpts)
+		% COSNEURONS/COSNEURONS Constructor for CosNeurons object - population of neurons with raised cosine tuning curves
+		% obj = CosNeurons(preferredStimuli, maxRate, backgroundRate, integrationTime, variabilityScheme, variabilityOpts)
 		%
-		% preferredStimulus - preferred stimulus value
+		% preferredStimuli - preferred stimulus value
+		% width - tuning curve width (this would be the variance if the curve was a probability distribution)
+		% maxRate - maximum firing rate (Hz)
 		% backgroundRate - background (spontaneous) firing rate (Hz)
 		% integrationTime - spike counting time per trial
-		% variability - a Variability object
+		% variabilityScheme - type of variability model
 		% variabilityOpts - vector of options
 		%
-		% preferredStimulus and backgroundFiringRate can be scalars or vectors of length popSize.
-		% CosNeurons accept only 1-D stimuli at present.
-		
-			if nargin ~= 5
-				error('Wrong number of arguments')
-			end
-			
-			obj = obj@Neurons(1, varargin{1}, 0, varargin{2}, varargin{3}, varargin{4}, varargin{5});
-		end
+		% preferredStimulus, maxFiringRate and backgroundFiringRate can be scalars or vectors of length popSize.
+		% CosNeurons accepts only 1-D stimuli at present.
+            
+            switch nargin
+                case 5
+                    preferredStimuli = varargin{1};
+                    backgroundRateIn = varargin{2};
+                    integrationTime = varargin{3};
+                    variabilityScheme = varargin{4};
+                    variabilityOpts = varargin{5};
+                otherwise
+                    error('Wrong number of inputs')
+            end
+            
+			% Superclass constructor
+            obj = obj@Neurons(1, preferredStimuli, integrationTime, variabilityScheme, variabilityOpts);
+            
+            if isscalar(backgroundRateIn) && isnumeric(backgroundRateIn)
+				obj.backgroundRate = double(backgroundRateIn(ones(obj.popSize, 1)));
+			elseif length(backgroundRateIn) == obj.popSize && isvector(backgroundRateIn) && isnumeric(backgroundRateIn)
+				obj.backgroundRate = reshape(double(backgroundRateIn), obj.popSize, 1);
+			else
+				error('Invalid background firing rate value or vector for population size %n', obj.popSize)
+            end
+        end
 		
 		function r = meanR(obj, stim)
 		% MEANR calculates mean responses to a set of stimuli
@@ -48,6 +66,9 @@ classdef CosNeurons < Neurons
 		end
 		
 		function dr = dMeanR(obj, stim)
+        % CIRCGAUSSNEURONS/DMEANR calculates the derivative of the tuning curve
+		% dr dr = dMeanR(obj, stim)
+
 			if ~isa(stim, 'StimulusEnsemble') 
 				error([inputname(2) ' is not a valid StimulusEnsemble object'])
 			end
@@ -60,6 +81,16 @@ classdef CosNeurons < Neurons
 			centre = repmat(obj.preferredStimulus, 1, stim.n);
 			
 			dr = pi/180 .* (1.16279 .* sin(degToRad(centre - stims)) .* double((cos(degToRad(stims - centre)) - 0.14 > 0.0)));
-		end
-	end
+        end
+        
+		function obj = remove(obj, nMarg)			
+			% Call superclass method
+			[obj margMask] = remove@Neurons(obj, nMarg);
+            
+            if length(obj.backgroundRate) > 1
+				obj.backgroundRate = obj.backgroundRate(margMask);
+            end
+        end
+        
+    end
 end
