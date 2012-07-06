ba6893718c5931837309c7d084d76a1d5d82251d
Stuart Yarrow <stu@outpost.org.uk>
2012-07-06 17:44:59 +0100

    Changed font size

--- auxiliary/singleEB.m
+++ auxiliary/singleEB.m
@@ -7,6 +7,6 @@ set(le, 'linestyle', '-', 'color', 'k', 'marker', 'none')
 line([x/(10^(width*0.5)), x, x*(10^(width*0.5))], [y y y], 'color', 'k')
 
 t = text(x * 10^(width * 1.5), y, label);
-set(t, 'fontsize', 8, 'verticalalignment', 'middle', 'horizontalalignment', 'left')
+set(t, 'fontsize', 9, 'verticalalignment', 'middle', 'horizontalalignment', 'left')
 
 h = [le t];
