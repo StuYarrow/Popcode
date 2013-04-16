80d2c41046b6f8a34fcbe4b464e57cd877c110e0
Stuart Yarrow <stu@outpost.org.uk>
2013-04-16 17:58:03 +0100

    Initial add of new stimulus classes

---
+++ popcode/ContinuousStimulusEnsemble.m
@@ -0,0 +1,24 @@
+classdef ContinuousStimulusEnsemble < StimulusEnsemble
+	
+	properties
+		continuous = true;
+        width = [];
+        lowerLimit = 0;
+        upperLimit = 0;
+	end
+	
+	methods
+
+		function obj = ContinuousStimulusEnsemble(varargin)
+		% ContinuousStimulusEnsemble/ContinuousStimulusEnsemble Constructor for ContinuousStimulusEnsemble object
+		% obj = ContinuousStimulusEnsemble()
+        %
+            
+            assert(nargin == 0, 'Wrong number of arguments')
+            
+			% Superclass constructor
+            obj = obj@StimulusEnsemble();
+        end
+                
+    end
+end
