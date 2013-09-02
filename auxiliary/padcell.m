84018d25aec392a8fae0a1cae62a7f40dcefb78a
Stuart Yarrow <stu@outpost.org.uk>
2013-09-01 17:13:52 -0700

    Removed aux scripts

--- auxiliary/padcell.m
+++
@@ -1,8 +0,0 @@
-function out = padcell(in, newSize)
-    out = cell(newSize);
-        
-    for el = 1 : numel(in)
-        [m n] = ind2sub(size(in), el);
-        out(m,n) = in(m,n);
-    end
-end
